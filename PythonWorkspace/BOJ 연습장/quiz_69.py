407fa200dc257165a1e80fa45972899205bf76a4
kimbap918 <fx887722@naver.com>
2022-07-26 00:21:55 +0900

    TIL 필기

--- PythonWorkspace/BOJ 연습장/quiz_69.py
+++ PythonWorkspace/BOJ 연습장/quiz_69.py
@@ -1,8 +1,8 @@
 def counting_stars(n):
-  if n == 1:
+  if n == 1: # 별 찍기
     return ['*']
 
-  stars=counting_stars(n//3)
+  stars = counting_stars(n//3) # 3의 거듭제곱 값들을 나눈 몫을 함수로 호출
   L = []
 
   for star in stars:
@@ -14,5 +14,5 @@ def counting_stars(n):
 
   return L
 
-N=int(input())
+N = int(input())
 print('\n'.join(counting_stars(N)))
---
+++ SWEA/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+        # if sise_L[price] > sise_L[-1] and cnt >= 1:
+        #     buy = sise_L[price]*cnt-buy
