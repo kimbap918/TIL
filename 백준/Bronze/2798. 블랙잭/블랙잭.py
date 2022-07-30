4fd56dc1c6548d4042edeb0dddb508351398d330
Choi <75712723+kimbap918@users.noreply.github.com>
2022-07-30 16:16:06 +0900

    [Bronze II] Title: 블랙잭, Time: 136 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,18 @@
+N, M = map(int, input().split())
+
+num = list(map(int, input().split()))
+sum = 0
+for i in range(len(num)):
+    for j in range(i+1, N):
+        for k in range(j+1, N):
+            # print("i :"+str(i))
+            # print("j :"+str(j))
+            # print("k :"+str(k))   
+            # 카드 3장 합이 M보다 작거나 카드 3장 합이 S보다 작으면 건너뜀          
+            if num[i]+num[j]+num[k] > M or num[i]+num[j]+num[k] < sum:
+                continue
+            # 그외에는 합산
+            sum = num[i]+num[j]+num[k]
+print(sum)
+
+
