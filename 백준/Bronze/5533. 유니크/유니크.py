b442a1c67ffc8f0febe47cf7cbf0d721d1aa28f4
Choi <75712723+kimbap918@users.noreply.github.com>
2022-08-03 22:43:51 +0900

    [Bronze I] Title: 유니크, Time: 92 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/5533. 유니크/유니크.py
@@ -0,0 +1,18 @@
+N = int(input())
+
+score = [list(map(int, input().split())) for _ in range(N)]
+
+for i in range(N):
+    sum = 0
+    for j in range(3):
+        a = score[i][j]
+        check = 1
+        for k in range(N):
+            if i == k:
+                continue
+            if a == score[k][j]:
+                check = 0
+                break
+        if check == 1:
+            sum += a
+    print(sum)
