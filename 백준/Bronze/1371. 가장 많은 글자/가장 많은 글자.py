9dbd6bc1cecec4f656782d3b14ca48e8b8fb2e1a
Choi <75712723+kimbap918@users.noreply.github.com>
2022-08-09 16:55:37 +0900

    [Bronze II] Title: 가장 많은 글자, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1371. 가장 많은 글자/가장 많은 글자.py
+++ 백준/Bronze/1371. 가장 많은 글자/가장 많은 글자.py
@@ -1,12 +1,22 @@
 import sys
-eng = 'abcdefghijklmnopqrstuvwxyz'
-result = []
-inp = sys.stdin.read()
+dict = {}
+a = []
+max = 0
+S = list(map(str, sys.stdin.read().replace(" ", "").replace("\n", "")))
+for char in S:
+    if char in dict:
+        dict[char] += 1
+    else:
+        dict[char] = 1
 
-for i in eng:
-    result.append(inp.count(i))
+for k, v in dict.items():
+    if max < v:
+        max = v
 
-m = max(result)
-for i in range(len(result)):
-    if m == result[i]:
-        print(chr(i+97), end = '')
+for k, v in dict.items():
+    if max == v:
+        a.append(k)
+
+a.sort()
+for i in a:
+    print(i, end='')
