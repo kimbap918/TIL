fae8876818161047e21526a2b19dec76fdfeaf34
Choi <75712723+kimbap918@users.noreply.github.com>
2022-08-21 20:06:14 +0900

    [Silver V] Title: 숫자 카드, Time: 804 ms, Memory: 114760 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+dic = {}
+card = list(map(int, input().split()))
+M = int(input())
+sang_card = list(map(int, input().split()))
+for i in sang_card:
+    dic[i] = 0
+
+for j in card:
+    if j in dic:
+        dic[j] = 1
+
+for v in dic.values():
+    print(v, end = ' ')
