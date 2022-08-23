7648536f7408c9d22a1bb443f0c7cd5a95e599f1
Choi <75712723+kimbap918@users.noreply.github.com>
2022-08-23 23:22:09 +0900

    [Bronze III] Title: 네 번째 점, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,13 @@
+x_nums = []
+y_nums = []
+for _ in range(3):
+    x, y = map(int, input().split())
+    x_nums.append(x)
+    y_nums.append(y)
+
+for i in range(3):
+    if x_nums.count(x_nums[i]) == 1:
+        x4 = x_nums[i]
+    if y_nums.count(y_nums[i]) == 1:
+        y4 = y_nums[i]
+print(x4, y4)
