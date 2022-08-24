03a2c30dc0599429e9bc9bde629dd13a1045669e
kimbap918 <fx887722@naver.com>
2022-08-24 12:25:52 +0900

    TIL SQL 모델링, ORM

--- PythonWorkspace/BOJ 연습장/tempCodeRunnerFile.py
+++ PythonWorkspace/BOJ 연습장/tempCodeRunnerFile.py
@@ -1,1 +1,11 @@
-S
+N, M = map(int, input().split())
+
+poket_dic = {input() : str(i) for i in range(1, N+1)}
+
+for j in range(M):
+    S = input()
+    for k, v in poket_dic.items():
+        if S == k:
+            print(v)
+        elif S == v:
+            print(k)
