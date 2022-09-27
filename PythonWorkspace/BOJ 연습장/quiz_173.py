c8d51b0d8f63ed58d501362def0c1185cbf62c2e
최준혁 <fx887722@naver.com>
2022-09-27 18:15:35 +0900

    파이썬 문제 풀이 RGB 거리, 계단 오르기, 정수 삼각형, 1로 만들기

--- PythonWorkspace/BOJ 연습장/quiz_173.py
+++ PythonWorkspace/BOJ 연습장/quiz_173.py
@@ -5,9 +5,10 @@ RGB = []
 for i in range(N):
     RGB.append(list(map(int, input().split())))
     # RGB = [[26, 40, 83]]
-
+print(RGB)
 for i in range(1, len(RGB)):
     RGB[i][0] += min(RGB[i-1][1], RGB[i-1][2]) # [0][1], [0][2] 중 최소값 누적
     RGB[i][1] += min(RGB[i-1][0], RGB[i-1][2])
     RGB[i][2] += min(RGB[i-1][0], RGB[i-1][1])
+print(RGB)
 print(min(RGB[N-1][0], RGB[N-1][1], RGB[N-1][2]))
--- PythonWorkspace/BOJ 연습장/quiz_174.py
+++ PythonWorkspace/BOJ 연습장/quiz_174.py
@@ -1,15 +1,17 @@
 n = int(input())
 dp = []
 
-for i in range(n) :                            ## 입력값 이차원리스트 형태로 dp테이블에 저장하기
+for i in range(n) :                            # 입력값 이차원리스트 형태로 dp테이블에 저장하기
     dp.append(list(map(int,input().split())))
 
-for i in range(1,n) :                           ## 행을 기준으로 for문 구성
-    for j in range(0,i+1) :                     ## 열을 기준으로 for문 구성
+for i in range(1,n) :                           # 행을 기준으로 for문 구성
+    for j in range(0,i+1) :                     # 열을 기준으로 for문 구성
         if j == 0 :
             dp[i][0] += dp[i-1][0]              # 0열끼리 더하기
+            # print(dp)
         elif j == i :
             dp[i][j] += dp[i-1][j-1]            # 마지막 열끼리 더하기
+            print(dp)
         else :
             dp[i][j] += max(dp[i-1][j-1],dp[i-1][j])    # 두 화살표중 더 큰 경우 받아들이기
 
