c32371096483127f7484a41f5244aaf56a52a5d6
Choi <75712723+kimbap918@users.noreply.github.com>
2022-09-28 21:48:09 +0900

    [Silver I] Title: 포도주 시식, Time: 540 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -0,0 +1,11 @@
+N = int(input())
+w = [0]
+for i in range(N):
+    w.append(int(input()))
+dp = [0]
+dp.append(w[1])
+if N > 1:
+    dp.append(w[1] + w[2])
+for i in range(3, N + 1):
+    dp.append(max(dp[i - 1], dp[i - 3] + w[i - 1] + w[i], dp[i - 2] + w[i]))
+print(dp[N])
