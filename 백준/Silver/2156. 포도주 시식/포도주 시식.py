e300492623359ea48946f39e14b73e54bb9f473c
Choi <75712723+kimbap918@users.noreply.github.com>
2022-10-02 01:05:08 +0900

    [Silver I] Title: 포도주 시식, Time: 468 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,11 +1,11 @@
 N = int(input())
-w = [0]
+wine = [0]
 for i in range(N):
-    w.append(int(input()))
+    wine.append(int(input())) # [0, 6, 10, 13, 9, 8, 1]
 dp = [0]
-dp.append(w[1])
-if N > 1:
-    dp.append(w[1] + w[2])
-for i in range(3, N + 1):
-    dp.append(max(dp[i - 1], dp[i - 3] + w[i - 1] + w[i], dp[i - 2] + w[i]))
+dp.append(wine[1]) # [0, 6]
+if N > 1: # 와인잔이 2개 이상일 경우
+    dp.append(wine[1] + wine[2]) # dp에 와인의 양 1, 2번 인덱스 값을 추가
+for i in range(3, N+1): # 3부터 N+1까지
+    dp.append(max(dp[i-1], dp[i-3] + wine[i-1] + wine[i], dp[i-2] + wine[i]))
 print(dp[N])
