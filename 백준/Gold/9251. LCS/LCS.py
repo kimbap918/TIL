832a053b9fe7fde7f89a62e645a43c50630bb609
Choi <75712723+kimbap918@users.noreply.github.com>
2022-10-02 15:58:03 +0900

    [Gold V] Title: LCS, Time: 632 ms, Memory: 55032 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+a = ' ' + input().rstrip()
+b = ' ' + input().rstrip()
+# print(a, b)
+dp = [[0] * len(b) for _ in range(len(a))]
+for i in range(1, len(a)):
+    for j in range(1, len(b)):
+        if a[i] == b[j]: 
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+# print(dp)
+print(dp[-1][-1]) # 2차원 배열 마지막의 마지막 요소를 가져옴
