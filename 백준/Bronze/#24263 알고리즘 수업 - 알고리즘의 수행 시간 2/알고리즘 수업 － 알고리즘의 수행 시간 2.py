96b67fcba78dec662075b3135d8865e512974e4c
Choi <75712723+kimbap918@users.noreply.github.com>
2023-03-19 18:26:56 +0900

    [24263] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 40 ms, Memory: 31256 KB -NKLCBHub

---
+++ 백준/Bronze/#24263 알고리즘 수업 - 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.py
@@ -0,0 +1,16 @@
+# A = []
+# def MenOfPassion(A, n):
+#    answer = 0
+#    for i in range(n):
+#        A.append(1)
+#        answer += A[i]
+#    return answer;
+
+# 주어진 함수의 for문이 항상 n에 비례하므로,
+# 수행 횟수는 n, 수행 시간은 항상 1이 된다.
+
+# n = int(input())
+# print(MenOfPassion(A, n))
+
+print(input())
+print(1)
