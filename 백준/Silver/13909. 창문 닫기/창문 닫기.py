dfcae4b53da1ffe92e34b79f5010ca482eb62d05
Choi <75712723+kimbap918@users.noreply.github.com>
2023-03-24 17:02:55 +0900

    [Silver V] Title: 창문 닫기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.py
@@ -0,0 +1,14 @@
+N = int(input())
+cnt = 0
+# 1부터 열려져 있는 창문 개수는 n이 제곱근일때 수가 증가한다
+if N == 1:
+    print(N)
+elif N == 2:
+    print(1)
+for i in range(1, N):
+    if i*i > N:
+        print(cnt)
+        break
+    cnt += 1
+
+
