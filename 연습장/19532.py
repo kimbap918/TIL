4cd60958decc873344f023beac6fa120d09c2dac
최준혁 <>
2023-03-24 15:46:18 +0900

    백준 문제풀이 골드바흐 파티션, 다음 소수

--- 연습장/19532.py
+++ 연습장/19532.py
@@ -1,6 +1,6 @@
 # 1*x + 3*y = -1
 # 4*x + 1*y = 7   
-# 
+# -999 부터 1000까지 x, y룰 전부 탐색
 a, b, c, d, e, f = map(int, input().split())
 
 for x in range(-999, 1000):
@@ -8,12 +8,4 @@ for x in range(-999, 1000):
         if ((a*x) + (b*y) == c) and ((d*x) + (e*y) == f):
             print(x, y)
 
-# while True:
-#     ans_1 = 0
-#     ans_2 = 0
-#     x, y = 
-#     if ans_1 == 1 and ans_2 == 1:
-#         break
-#     else:
-        
 
