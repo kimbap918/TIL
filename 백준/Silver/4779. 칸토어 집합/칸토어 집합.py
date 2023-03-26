0b53029684011e900f1fa3bff31676d5fdaf2ea5
Choi <75712723+kimbap918@users.noreply.github.com>
2023-03-26 13:57:22 +0900

    [Silver III] Title: 칸토어 집합, Time: 44 ms, Memory: 32292 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -1,6 +1,9 @@
 import sys
-for i in sys.stdin:
-    temp = "-"
+for i in sys.stdin: # ex) 3이면 i = 3
+    line = "-"
     for j in range(int(i)):
-        temp = temp+" "*len(temp)+temp
-    print(temp)
+        # 1회 - -
+        # 2회 - -   - -
+        # 3회 - -   - -         - -   - -
+        line = line+" "*len(line)+line
+    print(line)
