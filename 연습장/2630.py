3c4785ee77a23d703a981b1d4e447c4de788dace
최준혁 <>
2023-03-28 17:59:01 +0900

    백준 문제풀이 색종이 만들기

---
+++ 연습장/2630.py
@@ -0,0 +1,28 @@
+# 분할 정복 : 재귀적으로 자신을 호출하면서 그 연산의 단위를 조금씩 줄어가는 방식
+# 주어진 문제를 작은 사례로 나누고(Divide) 각각의 작은 문제들을 해결하여 정복(Conquer)
+import sys
+N = int(sys.stdin.readline()) # 종이 한 변의 길이
+# 정사각형 칸들의 색 상태
+paper = [list(map(int, sys.stdin.readline().split())) for _ in range(N)] 
+# 파란색, 흰색 결과값을 담을 리스트
+res = []
+
+def paper_cnt(x, y, N):
+    color = paper[x][y] # 색깔이 초기 값
+    for i in range(x, x+N):
+        for j in range(y, y+N):
+            if color != paper[i][j]:
+                paper_cnt(x, y, N//2)
+                paper_cnt(x, y+N//2, N//2)
+                paper_cnt(x+N//2, y, N//2)
+                paper_cnt(x+N//2, y+N//2, N//2)
+                return
+    if color == 0:
+        res.append(0)
+    else:
+        res.append(1)
+
+
+paper_cnt(0,0,N)
+print(res.count(0))
+print(res.count(1))
