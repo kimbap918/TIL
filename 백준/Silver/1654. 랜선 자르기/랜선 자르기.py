3dfe6e7d7c2c989925accb990d8a85876478002f
Choi <75712723+kimbap918@users.noreply.github.com>
2023-04-10 14:40:00 +0900

    [Silver II] Title: 랜선 자르기, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,18 +1,19 @@
 import sys
 input = sys.stdin.readline
 K, N = map(int, input().split())
-LAN = [int(input()) for i in range(K)]
-# 가장 짧은 길이, 가장 긴 길이
-start, end = 1, max(LAN)
+lines = [int(input()) for _ in range(K)]
+start, end = 1, max(lines)
 
-while start <= end:
-    mid = (start + end)//2
-    line = 0
-    for i in LAN:
-        line += i // mid
-    if line >= N:
-        start = mid + 1
-    else:
-        end = mid - 1
+def binary_search(start, end):
+    while start <= end:
+        mid = (start+end)//2
+        line = 0
+        for i in lines:
+            line += i // mid
+        if line >= N:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return end
 
-print(end)
+print(binary_search(start, end))
