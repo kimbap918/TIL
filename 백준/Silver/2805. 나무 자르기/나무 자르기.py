bf277f746338d49b9904e2dda87b69b77b8f5532
Choi <75712723+kimbap918@users.noreply.github.com>
2023-04-11 17:02:00 +0900

    [Silver II] Title: 나무 자르기, Time: 2180 ms, Memory: 143400 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,22 +1,20 @@
 import sys
 input = sys.stdin.readline
-
 N, M = map(int, input().split())
 logs = list(map(int, input().split()))
 start, end = 1, max(logs)
 
-while start <= end:
-    mid = (start+end) // 2
-    res = 0
+def binary_search(start, end):
+    while start <= end:
+        mid = (start+end) // 2
+        log = 0
+        for i in logs:
+            if i >= mid:
+                log += (i - mid)
+        if log >= M:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return end
 
-    for i in logs:
-        if i >= mid:
-            res += (i - mid)
-
-    if res >= M:
-        start = mid + 1
-    else:
-        end = mid - 1
-
-print(end)
-
+print(binary_search(start, end))
