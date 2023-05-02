789ed650926f3693bfab2b7ac6527384a04af750
최준혁 <fx887722@naver.com>
2023-05-02 16:35:14 +0900

    백준 문제풀이 유기농배추

---
+++ 연습장/1012.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+
+def BFS(x,y):           
+    queue = [(x,y)]
+    matrix[x][y] = 0 
+
+    while queue:
+        x,y = queue.pop(0)
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= M or ny < 0 or ny >= N:
+                continue
+            
+            if matrix[nx][ny] == 1 :
+                queue.append((nx,ny))
+                matrix[nx][ny] = 0
+
+T = int(input()) 
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+for i in range(T):
+    M, N, K = map(int, input().split())
+    matrix = [[0]*(N) for _ in range(M)]
+    cnt = 0
+
+    for j in range(K):
+        x,y = map(int, input().split())
+        matrix[x][y] = 1
+
+    for a in range(M):
+        for b in range(N):
+            if matrix[a][b] == 1:
+                BFS(a,b)
+                cnt += 1
+
+    print(cnt)
