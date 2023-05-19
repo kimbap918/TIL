2808d6a03405e2abb488da54973c025be5b1457a
최준혁 <fx887722@naver.com>
2023-05-19 15:33:18 +0900

    백준 문제풀이 거리두기, 벽 부수고 이동하기

--- 연습장/practice18.py
+++ 연습장/practice18.py
@@ -16,6 +16,7 @@ N = int(input())
 dp = [[0] * 5 for _ in range(N+1)]
 dp[0][0] = 1  # 초기값 설정
 mod = 100_000_007
+print(dp)
 
 for i in range(1, N+1):
     dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2] + dp[i-1][3] + dp[i-1][4]) % mod
