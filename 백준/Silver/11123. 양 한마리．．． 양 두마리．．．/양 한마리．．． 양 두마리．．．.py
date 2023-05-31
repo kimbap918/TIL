d43e244230c42c6f4e8fe4406adca000a447783d
Choi <75712723+kimbap918@users.noreply.github.com>
2023-05-31 13:14:21 +0900

    [Silver II] Title: 양 한마리... 양 두마리..., Time: 204 ms, Memory: 32740 KB -BaekjoonHub

--- 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.py
+++ 백준/Silver/11123. 양 한마리．．． 양 두마리．．．/양 한마리．．． 양 두마리．．．.py
@@ -1,24 +1,20 @@
-from collections import deque
+
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 T = int(input())
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
-def BFS(h, w):
-    global cnt
-    Q = deque([[h, w]])
+def DFS(h, w):
+    visited[h][w] = True
 
-    while Q:
-        y, x = Q.popleft()
-        for i in range(4):
-            ny = y + dy[i]
-            nx = x + dx[i]
-            if 0 <= nx < W and 0 <= ny < H and graph[ny][nx] == '#' and not visited[ny][nx]:
-                visited[ny][nx] = True
-                Q.append([ny, nx])
-
+    for i in range(4):
+        ny = h + dy[i]
+        nx = w + dx[i]
+        if 0 <= nx < W and 0 <= ny < H and graph[ny][nx] == '#' and not visited[ny][nx]:
+            DFS(ny, nx)
 
 for _ in range(T):
     cnt = 0
@@ -29,7 +25,6 @@ for _ in range(T):
     for i in range(H):
         for j in range(W):
             if graph[i][j] == '#' and not visited[i][j]:
-                visited[i][j] = True
-                BFS(i, j)
+                DFS(i, j)
                 cnt += 1
     print(cnt)
