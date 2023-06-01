a8228eb90724454dc7dcb5919226587f1921916c
Choi <75712723+kimbap918@users.noreply.github.com>
2023-06-01 15:29:16 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 436 ms, Memory: 40480 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,6 +1,7 @@
+
 import sys
+from collections import deque
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
 N, M = map(int, input().split())
 campus = [list(input().rstrip()) for _ in range(N)]
@@ -9,26 +10,30 @@ dx = [0, 0, 1, -1]
 dy = [-1, 1, 0, 0]
 cnt = 0
 
-def DFS(i, j):
+def BFS(i, j):
     global cnt
-    
-    for k in range(4):
-        nx = j + dx[k]
-        ny = i + dy[k]
+    Q = deque([[i, j]])
 
-        if 0 <= nx < M and 0 <= ny < N and not visited[ny][nx]:
-            if campus[ny][nx] == "X":
-                continue
-            if campus[ny][nx] == "P":
-                cnt += 1
-            visited[ny][nx] = True
-            DFS(ny, nx)
+    while Q:
+        y, x = Q.popleft()
+
+        for k in range(4):
+            nx = x + dx[k]
+            ny = y + dy[k]
+
+            if 0 <= nx < M and 0 <= ny < N and not visited[ny][nx]:
+                if campus[ny][nx] == "X":
+                    continue
+                if campus[ny][nx] == "P":
+                    cnt += 1
+                visited[ny][nx] = True
+                Q.append([ny, nx])
 
 for i in range(N):
     for j in range(M):
         if campus[i][j] == "I" and not visited[i][j]:
             visited[i][j] = True
-            DFS(i, j)
+            BFS(i, j)
 
 if cnt:
     print(cnt)
