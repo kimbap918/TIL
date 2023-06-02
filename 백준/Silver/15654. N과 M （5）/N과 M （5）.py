c3b03ecd8110d19c0ef555bf7350c450f0163331
Choi <75712723+kimbap918@users.noreply.github.com>
2023-06-02 12:30:41 +0900

    [Silver III] Title: N과 M (5), Time: 160 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+visited = [False] * N
+ans = []
+
+arr.sort()
+
+def backtraking(depth, N, M):
+    if depth == M:
+        print(' '.join(map(str, ans)))
+        return
+    for i in range(N):
+        if not visited[i]:
+            visited[i] = True
+            ans.append(arr[i])
+            backtraking(depth+1, N, M)
+            # 백트래킹
+            ans.pop()
+            visited[i] = False
+
+
+backtraking(0, N, M)
