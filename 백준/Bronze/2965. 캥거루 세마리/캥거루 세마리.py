404384e881de464c4837690de364e175b68d6d8f
Choi <75712723+kimbap918@users.noreply.github.com>
2024-02-27 16:17:26 +0900

    [Bronze III] Title: 캥거루 세마리, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2965. 캥거루 세마리/캥거루 세마리.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+A, B, C = map(int, input().split())
+cnt = 0
+
+while True:
+    if B-A == C-B and B-A == 1:
+        break
+
+    if B-A <= C-B:
+        A = B
+        B = C-1
+        cnt += 1
+    elif B-A >= C-B:
+        C = B
+        B = C-1
+        cnt += 1
+
+
+print(cnt)
