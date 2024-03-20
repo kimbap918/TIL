8e66e666f86a758040d1bfdfb78de57e016a58d7
Choi <75712723+kimbap918@users.noreply.github.com>
2024-03-20 21:54:05 +0900

    [Bronze III] Title: 베시와 데이지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/16431. 베시와 데이지/베시와 데이지.py
@@ -0,0 +1,11 @@
+Br, Bc = map(int, input().split())
+Dr, Dc = map(int, input().split())
+Jr, Jc = map(int, input().split())
+B = max(abs(Jr-Br), abs(Jc-Bc))
+D = abs(Jr-Dr) + abs(Jc-Dc)
+if B == D:
+    print("tie")
+elif B < D:
+    print("bessie")
+else:
+    print("daisy")
