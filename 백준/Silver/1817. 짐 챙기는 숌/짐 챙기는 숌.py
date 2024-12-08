9012dfa93e0cdba72802f60e1c16dc208290f7fc
Choi <75712723+kimbap918@users.noreply.github.com>
2024-12-08 20:43:59 +0900

    [Silver V] Title: 짐 챙기는 숌, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1817. 짐 챙기는 숌/짐 챙기는 숌.py
@@ -0,0 +1,18 @@
+N, M = map(int, input().split())
+if N != 0:
+    books = list(input().split())
+    box = 1
+    weight = 0
+
+    for i in range(len(books)-1):
+        if weight < M:
+            weight += int(books[i])
+
+            if weight + int(books[i+1]) > M:
+                box += 1
+                weight = 0
+
+else:
+    box = 0
+
+print(box)
