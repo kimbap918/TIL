5836263f0f5568a07b394f745d84da0ad655938a
Choi <75712723+kimbap918@users.noreply.github.com>
2024-12-12 18:38:44 +0900

    [Silver V] Title: 점프는 즐거워, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/4383. 점프는 즐거워/점프는 즐거워.py
+++ 백준/Silver/4383. 점프는 즐거워/점프는 즐거워.py
@@ -1,22 +1,15 @@
 while True:
     try:
-        # 입력받고 분리
         jumper = list(map(int, input().split()))
         N = jumper[0]
-        sequence = jumper[1:]
+        res = []
+        # 1부터 n-1까지 모두 나와야 Jolly jumper
+        for i in range(1, len(jumper)-1):
+            res.append(abs(jumper[i] - jumper[i+1]))
 
-        # 길이가 1인 경우 무조건 Jolly
-        if N == 1:
-            print("Jolly")
-            continue
-
-        # 절댓값 차이를 집합으로 저장
-        differences = set(abs(sequence[i] - sequence[i + 1]) for i in range(N - 1))
-
-        # 1부터 N-1까지 차이를 모두 포함하는지 확인
-        if differences == set(range(1, N)):
+        if sum(set(res)) == int((N*(N+1)/2)-N):
             print("Jolly")
         else:
             print("Not jolly")
-    except EOFError:
+    except:
         break
