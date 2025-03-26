13b8a9daedb4ad2899d448ab6623fca3c07bd508
Choi <75712723+kimbap918@users.noreply.github.com>
2025-03-26 18:09:42 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 88 ms, Memory: 36872 KB -BaekjoonHub

---
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [int(input()) for _ in range(N)]
+cnt = 0
+stack = []
+
+for h in arr:
+    # 10 3 8 4 12 2
+    while stack and stack[-1] <= h: # 스택의 마지막이 높이보다 작거나 같을때까지
+
+        stack.pop()
+    cnt += len(stack)
+    stack.append(h)
+
+print(cnt)
