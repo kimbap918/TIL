ad870bca66fe9389130c9856039c5ff72376dd05
Choi <75712723+kimbap918@users.noreply.github.com>
2025-03-29 20:29:47 +0900

    [Silver II] Title: N과 M (9), Time: 68 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,24 @@
+N, M = map(int, input().split())
+arr = sorted(map(int, input().split()))  # 중복 제거 X, 정렬만 수행
+visited = [False] * N
+res = []
+
+
+def dfs(depth):
+    if depth == M:
+        print(' '.join(map(str, res)))
+        return
+
+    prev = -1
+
+    for i in range(N): # 0부터 숫자개수까지
+        if not visited[i] and prev != arr[i]:
+            visited[i] = True
+            res.append(arr[i])
+            prev = arr[i]
+            dfs(depth+1)
+            visited[i] = False
+            res.pop()
+
+
+dfs(0)
