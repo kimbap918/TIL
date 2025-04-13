056e5a22f517bffed6856ad592d8f820e8b71c2c
Choi <75712723+kimbap918@users.noreply.github.com>
2025-04-13 18:15:25 +0900

    [Bronze II] Title: 벌집, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,16 +1,9 @@
-# 등차수열 
-# N번째 항의 값 = 시작숫자 + (시작숫자-1) * 공차
-start = 1 # 시작지점(시작숫자)
-res = 0 # 도착까지 횟수 
-goal = int(input()) # N번째 항의 값, 목표지점
-a = []
+N = int(input())
 
-for i in range(0, goal):
-    start += res * 6
-    a.append(start)
-    if start < goal:
-        res += 1
-    else:
-        res += 1
-        break
-print(res)
+if N == 1:
+    print(1)
+else:
+    L = 1
+    while 1 + 3 * L * (L - 1) < N:
+        L += 1
+    print(L)
