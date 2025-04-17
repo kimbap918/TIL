cd5dae46ec3332c0f4b71ec29c45791de4611f7d
Choi <75712723+kimbap918@users.noreply.github.com>
2025-04-17 18:45:16 +0900

    [Silver V] Title: 올림픽, Time: 60 ms, Memory: 34944 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -3,38 +3,21 @@
 # 3. 금, 은메달 수가 모두 같으면, 동메달 수가 더 많은 나라
 
 import sys
+from collections import defaultdict
 input = sys.stdin.readline
 
+
 N, K = map(int, input().split())
-nations = []
+nations = defaultdict(list)
 
 for i in range(N):
-    nation = list(map(int, input().split()))
-    nations.append(nation)
+    r, g, s, b = list(map(int, input().split()))
+    nations[g, s, b].append(r)
 
-
-nations.sort(key=lambda x : (-x[1], -x[2], -x[3]))
-
-
+record = sorted(nations.keys(), reverse=True)
 rank = 1
-same_rank = 1
-prev = nations[0][1:]
-rank_dict = {nations[0][0] : 1}
 
-
-# print(rank_dict)
-
-for i in range(1, N):
-    curr = nations[i][1:]
-    if curr == prev:
-        rank_dict[[i][0]] = rank
-        same_rank += 1
-
-    else:
-        rank += same_rank
-        same_rank = 1
-        prev = curr
-        rank_dict[[i][0]] = rank
-
-
-print(rank_dict[K-1])
+for i in record:
+    if K in nations[i]:
+        print(rank)
+    rank += len(nations[i])
